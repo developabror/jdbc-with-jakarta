f2588f34632c688666050393b9aa29613514138a
Abror <developabror@gmail.com>
2024-08-01 18:18:45 +0500

    test

--- src/main/java/uz/app/jdbcapp/controller/SignIn.java
+++ src/main/java/uz/app/jdbcapp/controller/SignIn.java
@@ -22,7 +22,7 @@ public class SignIn extends HttpServlet {
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String dbAddress = getServletConfig().getInitParameter("db_address");
         req.setAttribute("sign", false);
-        req.getRequestDispatcher("views/auth.jsp").forward(req, resp);
+        req.getRequestDispatcher("views/index.jsp").forward(req, resp);
     }
 
 
